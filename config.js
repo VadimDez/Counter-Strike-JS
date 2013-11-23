77b220bebbc8ddfa4a778f5bcc89eb52dbf3cf39
Mathias <mathias.vp@hotmail.com>
2013-11-23 01:20:06 +0100

    Added config file and Pointer Lock API abstraction class

---
+++ config.js
@@ -0,0 +1,18 @@
+/**
+	This file contains all configuration settings for the engine
+**/
+
+window.cs = window.cs || { };
+
+cs.config = {
+	MAP_PATH: "data/maps/cs_assault.bsp",
+	NEAR_CLIPPING: 0.1,
+	FAR_CLIPPING: 10000.0,
+	FIELD_OF_VIEW: 59.0, //In degrees
+	PLAYER_HEIGHT: 17,
+	GRAVITY: 12,
+	//How much the player can change his Z position without a jump.
+	MAX_Z_CHANGE: 17,
+	//The factor multiplied to the delta x and delta y of the mouse movement
+	MOUSE_SENSITIVITY: 0.0025
+};
