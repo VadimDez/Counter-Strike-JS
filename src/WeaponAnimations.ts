0da2e640a94534b1dcb0eb12967050fc3369d13d
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2019-01-04 22:43:18 +0100

    animation

--- src/WeaponAnimations.ts
+++ src/WeaponAnimations.ts
@@ -1,167 +1,225 @@
 export const WeaponAnimations = {
-  m3: [{
-    idle: 0,
-    shoot: [1, 2],
-    reload: [5, 3, 4],
-    draw: [6]
-  }],
-  xm1014: [{
-    idle: 0,
-    shoot: [1, 2],
-    reload: [5, 3, 4],
-    draw: [6]
-  }],
-  
-  tmp: [{
-    idle: 0,
-    shoot: [3, 4, 5],
-    reload: [1],
-    draw: [2]
-  }],
-  mac10: [{
-    idle: 0,
-    shoot: [3, 4, 5],
-    reload: [1],
-    draw: [2]
-  }],
-  mp5: [{
-    idle: 0,
-    shoot: [3, 4, 5],
-    reload: [1],
-    draw: [2]
-  }],
-  ump45: [{
-    idle: 0,
-    shoot: [3, 4, 5],
-    reload: [1],
-    draw: [2]
-  }],
-  p90: [{
-    idle: 0,
-    shoot: [3, 4, 5],
-    reload: [1],
-    draw: [2]
-  }],
+  knife: [
+    {
+      idle: 0,
+      shoot: 6,
+      special: 5,
+      draw: [3]
+    }
+  ],
+  m3: [
+    {
+      idle: 0,
+      shoot: [1, 2],
+      reload: [5, 3, 4],
+      draw: [6]
+    }
+  ],
+  xm1014: [
+    {
+      idle: 0,
+      shoot: [1, 2],
+      reload: [5, 3, 4],
+      draw: [6]
+    }
+  ],
 
-  glock18: [{
-    idle: 0,
-    shoot: [3, 4, 5],
-    reload: [7],
-    draw: [8],
-  }, {
-    idle: 0,
-    shoot: [],
-    reload: [12],
-    draw: [11],
-  }],
-  usp: [{
-    idle: 8,
-    shoot: [9, 10, 11],
-    reload: [13],
-    draw: [14],
-    special: [7]
-  }, {
-    idle: 0,
-    shoot: [1, 2, 3],
-    reload: [5],
-    draw: [6],
-    special: [15]
-  }],
-  p228: [{
-    idle: 0,
-    shoot: [1, 2, 3],
-    reload: [5],
-    draw: [6]
-  }],
-  deagle: [{
-    idle: 0,
-    shoot: [1, 2],
-    reload: [4],
-    draw: [5]
-  }],
-  fiveseven: [{
-    idle: 0,
-    shoot: [1, 2],
-    reload: [4],
-    draw: [5]
-  }],
-  elite: [{
-    idle: 0,
-    shoot: [2, 3, 4, 5, 6],
-    reload: [14],
-    draw: [15]
-  }, {
-    idle: 0,
-    shoot: [8, 9, 10, 11, 12],
-    reload: [14],
-    draw: [15]
-  }],
-  
-  galil: [{
-    idle: 0,
-    shoot: [3, 4, 5],
-    reload: [1],
-    draw: [2]
-  }],
-  famas: [{
-    idle: 0,
-    shoot: [3, 4, 5],
-    reload: [1],
-    draw: [2]
-  }],
-  ak47: [{
-    idle: 0,
-    shoot: [3, 4, 5],
-    reload: [1],
-    draw: [2]
-  }],
-  m4a1: [{
-    idle: 7,
-    shoot: [8, 9, 10],
-    reload: [11],
-    draw: [12],
-    special: [6]
-  }, {
-    idle: 0,
-    shoot: [1, 2, 3],
-    reload: [4],
-    draw: [5],
-    special: [13]
-  }],
-  sg552: [{
-    idle: 0,
-    shoot: [3, 4, 5],
-    reload: [1],
-    draw: [2]
-  }],
-  aug: [{
-    idle: 0,
-    shoot: [3, 4, 5],
-    reload: [1],
-    draw: [2]
-  }],
-  scout: [{
-    idle: 0,
-    shoot: [1],
-    reload: [2],
-    draw: [3]
-  }],
-  sg550: [{
-    idle: 0,
-    shoot: [1, 2],
-    reload: [3],
-    draw: [4]
-  }],
-  awp: [{
-    idle: 0,
-    shoot: [1, 2, 3],
-    reload: [4],
-    draw: [5]
-  }],
-  g3sg1: [{
-    idle: 0,
-    shoot: [1, 2],
-    reload: [3],
-    draw: [4]
-  }]
+  tmp: [
+    {
+      idle: 0,
+      shoot: [3, 4, 5],
+      reload: [1],
+      draw: [2]
+    }
+  ],
+  mac10: [
+    {
+      idle: 0,
+      shoot: [3, 4, 5],
+      reload: [1],
+      draw: [2]
+    }
+  ],
+  mp5: [
+    {
+      idle: 0,
+      shoot: [3, 4, 5],
+      reload: [1],
+      draw: [2]
+    }
+  ],
+  ump45: [
+    {
+      idle: 0,
+      shoot: [3, 4, 5],
+      reload: [1],
+      draw: [2]
+    }
+  ],
+  p90: [
+    {
+      idle: 0,
+      shoot: [3, 4, 5],
+      reload: [1],
+      draw: [2]
+    }
+  ],
+
+  glock18: [
+    {
+      idle: 0,
+      shoot: [3, 4, 5],
+      reload: [7],
+      draw: [8]
+    },
+    {
+      idle: 0,
+      shoot: [],
+      reload: [12],
+      draw: [11]
+    }
+  ],
+  usp: [
+    {
+      idle: 8,
+      shoot: [9, 10, 11],
+      reload: [13],
+      draw: [14],
+      special: [7]
+    },
+    {
+      idle: 0,
+      shoot: [1, 2, 3],
+      reload: [5],
+      draw: [6],
+      special: [15]
+    }
+  ],
+  p228: [
+    {
+      idle: 0,
+      shoot: [1, 2, 3],
+      reload: [5],
+      draw: [6]
+    }
+  ],
+  deagle: [
+    {
+      idle: 0,
+      shoot: [1, 2],
+      reload: [4],
+      draw: [5]
+    }
+  ],
+  fiveseven: [
+    {
+      idle: 0,
+      shoot: [1, 2],
+      reload: [4],
+      draw: [5]
+    }
+  ],
+  elite: [
+    {
+      idle: 0,
+      shoot: [2, 3, 4, 5, 6],
+      reload: [14],
+      draw: [15]
+    },
+    {
+      idle: 0,
+      shoot: [8, 9, 10, 11, 12],
+      reload: [14],
+      draw: [15]
+    }
+  ],
+
+  galil: [
+    {
+      idle: 0,
+      shoot: [3, 4, 5],
+      reload: [1],
+      draw: [2]
+    }
+  ],
+  famas: [
+    {
+      idle: 0,
+      shoot: [3, 4, 5],
+      reload: [1],
+      draw: [2]
+    }
+  ],
+  ak47: [
+    {
+      idle: 0,
+      shoot: [3, 4, 5],
+      reload: 1,
+      draw: 2
+    }
+  ],
+  m4a1: [
+    {
+      idle: 7,
+      shoot: [8, 9, 10],
+      reload: [11],
+      draw: [12],
+      special: [6]
+    },
+    {
+      idle: 0,
+      shoot: [1, 2, 3],
+      reload: [4],
+      draw: [5],
+      special: [13]
+    }
+  ],
+  sg552: [
+    {
+      idle: 0,
+      shoot: [3, 4, 5],
+      reload: [1],
+      draw: [2]
+    }
+  ],
+  aug: [
+    {
+      idle: 0,
+      shoot: [3, 4, 5],
+      reload: [1],
+      draw: [2]
+    }
+  ],
+  scout: [
+    {
+      idle: 0,
+      shoot: [1],
+      reload: [2],
+      draw: [3]
+    }
+  ],
+  sg550: [
+    {
+      idle: 0,
+      shoot: [1, 2],
+      reload: [3],
+      draw: [4]
+    }
+  ],
+  awp: [
+    {
+      idle: 0,
+      shoot: [1, 2, 3],
+      reload: [4],
+      draw: [5]
+    }
+  ],
+  g3sg1: [
+    {
+      idle: 0,
+      shoot: [1, 2],
+      reload: [3],
+      draw: [4]
+    }
+  ]
 };
