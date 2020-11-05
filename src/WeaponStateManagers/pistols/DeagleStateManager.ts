f49accf366ebbb8a9e2408ac7e9cf64a81b8ee5e
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2020-11-05 22:36:13 +0100

    minor

--- src/WeaponStateManagers/pistols/DeagleStateManager.ts
+++ src/WeaponStateManagers/pistols/DeagleStateManager.ts
@@ -1,6 +1,9 @@
+import { Weapon } from '../../Weapon';
 import { PistolStateManager } from './PistolStateManager';
 
 export class DeagleStateManager extends PistolStateManager {
   ammo = 7;
   magazineCapacity = 7;
+
+  onSpecial(weapon: Weapon) {}
 }
